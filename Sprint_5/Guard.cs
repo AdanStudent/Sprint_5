27b6689639185f8d5e5abe1eb306ed18c83f6713
crosbyn <32074054+crosbyn@users.noreply.github.com>
2019-03-11 13:28:39 -0500

    Getting WPF_Guard setup

---
+++ Sprint_5/Guard.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sprint_5
+{
+    public class Guard
+    {
+        public float Hours { get; set; }
+
+        public string Name { get; set; }
+
+        private bool IsWorking;
+
+        private int MaxHours;
+
+        public Guard(float h, string n)
+        {
+            Hours = h;
+            Name = n;
+            MaxHours = 40;
+            this.IsWorking = false;
+        }
+
+        public void ClockIn()
+        {
+            if (!IsWorking && Hours <= MaxHours)
+            {
+                this.IsWorking = true;
+            }
+        }
+
+        public void ClockOut()
+        {
+            if (IsWorking || Hours <= MaxHours)
+            {
+                this.IsWorking = false;
+            }
+        }
+
+        public string LogHours()
+        {
+            return $"{this.Name} has worked {this.Hours} this week.";
+        }
+
+    }
+}
---
+++ Sprint_5/WPF_Guard.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Sprint_5
+{
+    public class WPF_Guard : DependencyObject, INotifyPropertyChanged
+    {
+        private Guard
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
